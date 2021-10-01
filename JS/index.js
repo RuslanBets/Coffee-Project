97e0e30d61314b0093c6d3731e03a742c394dac2
Ruslan Bets <betsruslanjs@gmail.com>
2021-10-01 21:10:00 +0300

    error

--- JS/index.js
+++ JS/index.js
@@ -3,6 +3,25 @@ window.onload = function top() {
   let burger = document.querySelector('.burger')
   let menu = document.querySelector('.menu-contacts')
   let closeBtn = document.querySelector('.close-btn')
+  let time = 1000
+  let step = 100
+
+  function outNum (num, elem) {
+    let l = document.querySelector('#'+ elem);
+    n = 0
+    let t = Math.round(time / (num / step));
+    let interval = setInterval(() => {
+      n = n + step;
+      if (n === num) {
+        clearInterval(interval)
+      }
+      l.innerHTML = n;
+    },
+      t)
+  }
+
+  // outNum(2536, 'out-1')
+
  
   
   window.onscroll = function(){
